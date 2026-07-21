dcc23814d876749f7e4b202c10a7077959d2615e
Vickykiu <vickykiu@1utar.my>
2026-07-21 23:43:14 +0800

    Classify HTML-only PHP pages as HTML

--- LOOO electronics/main page.php
+++ LOOO electronics/main page.php
@@ -27,10 +27,11 @@ if ($isLoggedIn && !empty($userName)) {
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" dir="ltr" translate="no">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Language" content="en">
     <title>LOOO - Electronics & Computers</title>
     <style>
         * {
